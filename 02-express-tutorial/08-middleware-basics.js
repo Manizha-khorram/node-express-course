7eb774ee2934021137121497de24a4af5d0531e3
Manizha-khorram <108601629+Manizha-khorram@users.noreply.github.com>
2022-11-16 02:13:37 -0500

    answers for lesson 5

--- 02-express-tutorial/08-middleware-basics.js
+++ 02-express-tutorial/08-middleware-basics.js
@@ -1,35 +1,23 @@
-const express = require ('express');
-const app = express();
-const morgan = require ('morgan')
-const logger = require('./logger')
-const authorize = require('./authorize')
-// req => middleware => res
-//app.use([logger , authorize])
-//app.use(express.static('./public'))
-app.use(morgan('tiny'))
+const express = require('express')
+const app = express()
 
-app.get('/' , logger ,(req , res) => {
-     
-      res.send('Home')
+//  req => middleware => res
+
+const logger = (req, res, next) => {
+  const method = req.method
+  const url = req.url
+  const time = new Date().getFullYear()
+  console.log(method, url, time)
+  next()
+}
+
+app.get('/', logger, (req, res) => {
+  res.send('Home')
 })
-app.get('/about' , (req , res) => {
-
+app.get('/about', logger, (req, res) => {
   res.send('About')
 })
 
-app.get('/api/products' , (req , res) => {
-
-    res.send('Products')
-  })
-
-  //if I want to use the middleware for single route just I can add  app.get('/api/items',[logger , authorize],(req , res) => {
-app.get('/api/items' , (req , res) => {
-  //using that middleware for item , by typing localhost:5000/api/items/?user=john I can access items.
-    console.log(req.user)
-    res.send('Items')
-  })
-
 app.listen(5000, () => {
-    console.log('Server is listening on port 5000....')
-  })
-  
+  console.log('Server is listening on port 5000....')
+})
